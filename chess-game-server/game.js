6be1006470acaf13c97f252c39e424f8cf2691d2
Ramsaiharibabu <94447207+Ramsaiharibabu@users.noreply.github.com>
2024-08-27 11:32:40 +0530

    Delete chess-game-server/game.js

--- chess-game-server/game.js
+++
@@ -1,9 +0,0 @@
-// const mongoose = require('mongoose');
-
-// const gameSchema = new mongoose.Schema({
-//   board: Array,
-//   players: Object,
-//   status: String, // ongoing, finished, etc.
-// });
-
-// module.exports = mongoose.model('Game', gameSchema);
